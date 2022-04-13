ddd1639467bce630eee3f73f6067874bdaf8e5ee
Martin Nogu <martin.nogu22@gmail.com>
2022-04-13 22:22:14 +0300

    add new projects

---
+++ DirectorisAndFiles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace DirectorisAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string roootDirectory = @"C:\Users\Priit\source\samples";
+            Console.WriteLine("Enter directory name:");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name:");
+            string fileName = Console.ReadLine();
+
+            //File.Create($"{roootDirectory}\\{newDirectory}\\{fileName}");
+
+            if (Directory.Exists($"{roootDirectory}\\{newDirectory}") && File.Exists($"{roootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and File exist.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{roootDirectory}\\{newDirectory}");
+                File.Create($"{roootDirectory}\\{newDirectory}\\{fileName}");
+            }
+        }
+    }
+}
---
+++ ShoppingListvol2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingListvol2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\Priit\source\samples\ShoppingList";
+            string fileName = @"\\myShoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add something to a shopping list? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter what you would like to add to the list:");
+                    string userShopping = Console.ReadLine();
+                    myShoppingList.Add(userShopping);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Take care!");
+                }
+            }
+            Console.Clear();
+
+            foreach (string list in myShoppingList)
+            {
+                Console.WriteLine($"In youre list: {list}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myShoppingList);
+        }
+    }
+}
